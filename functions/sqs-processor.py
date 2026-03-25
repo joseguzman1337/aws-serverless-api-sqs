b6b280d5adee60ca12baea921a06b22539b9a9e2
joseguzman1337 <joseguzman1337@users.noreply.github.com>
2026-03-24 23:21:43 -0500

    Revert "chore: automated swarm-wide synchronization"
    This reverts commit 51c19753f8563f3524d45b24285121ee497c25aa.

---
+++ functions/sqs-processor.py
@@ -0,0 +1,33 @@
+import json
+from aws_lambda_powertools import (
+    Logger, Tracer
+)
+from aws_lambda_powertools.utilities.batch import sqs_batch_processor    
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+from aws_xray_lambda_segment_shim import get_sqs_triggered_recorder
+
+logger = Logger()
+tracer = Tracer(patch_modules=[ "boto3" ])
+lambda_context: LambdaContext = {}
+
+# log record and complete
+def process_it(record):
+    trace_header = record.get('attributes', {}).get('AWSTraceHeader', '')
+    logger.info('processing it')
+    logger.info(record)
+    logger.info(trace_header)
+    recorder = get_sqs_triggered_recorder(
+        record=record,
+        lambda_request_id=lambda_context.aws_request_id,
+        lambda_arn=lambda_context.invoked_function_arn
+    )
+    
+    return True
+
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
+@sqs_batch_processor(record_handler=process_it)
+def handler(event, context):
+    return True
+
